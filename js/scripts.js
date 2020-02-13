bd95b8db2b56c110cab01662172da5f5f4a46fe9
mattandvivian <street.gal@gmail.com>
2020-02-13 16:25:06 -0400

    Style fix: zoomed out a tad in maps

--- js/scripts.js
+++ js/scripts.js
@@ -252,7 +252,7 @@ function initMap() {
     var end = new google.maps.LatLng(44.104587, -77.061417);
 
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
-      zoom: 10,
+      zoom: 9,
       center: start
     });
 
